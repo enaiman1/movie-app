125f8f149d91a1e9b2461fec01ed1175b4174da4
Ericcentrik <enaiman1@gmail.com>
2019-12-12 20:20:00 -0500

    added a backdrop function to change background when modal appears

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,16 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
+const backdrop = document.getElementById("backdrop")
+
+// create a backdrop when modal appears
+const toggleBackdrop = () => {
+    backdrop.classList.toggle("visible");
+}
 
 // this function will open and close modal
 const toggleMovieModal = () =>{
 addMovieModal.classList.toggle("visible");
+toggleBackdrop();
 }
 
 
