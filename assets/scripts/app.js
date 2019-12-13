ee5383ed71262bd7ad60d3375b24ca0d24e4af6e
Ericcentrik <enaiman1@gmail.com>
2019-12-12 20:50:07 -0500

    created a function to take the value from the modal as well as validate user put inputed befor hiting confirm

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,9 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop")
-const cancelAddMovieButtom = addMovieModal.querySelector('.btn--passive');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input')
 
 // create a backdrop when modal appears
 const toggleBackdrop = () => {
@@ -9,13 +11,29 @@ const toggleBackdrop = () => {
 }
 
 // this function will open and close modal
-const toggleMovieModal = () =>{
-addMovieModal.classList.toggle("visible");
-toggleBackdrop();
+const toggleMovieModal = () => {
+    addMovieModal.classList.toggle("visible");
+    toggleBackdrop();
 }
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
     toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if (
+        titleValue.trim() === "" ||
+        imageUrlValue.trim() === "" ||
+        ratingValue.trim() === "" ||
+        +ratingValue < 1 ||
+        +ratingValue > 5
+        ) {
+            alert("please enter valid values (rating between 1 and 5")
+    }
 }
 
 // this function will toggle between a dark background when modal displays
@@ -26,8 +44,11 @@ const backdropClickHandler = () => {
 // event listener to display modal to add new movie
 startAddMovieButton.addEventListener("click", toggleMovieModal);
 
-// allows you to click on background to make modal disapper
-backdrop.addEventListener("click", toggleMovieModal)
+// allows you to click on background to make modal disappear
+backdrop.addEventListener("click", backdropClickHandler)
 
 //allows the cancle button to make modal disapper
-cancelAddMovieButtom.addEventListener("click", cancelAddMovie)
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler)
+
+//allow the confirm button to take input and make modal disappear
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
